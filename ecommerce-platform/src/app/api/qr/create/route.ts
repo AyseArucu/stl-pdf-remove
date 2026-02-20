3a952323dfb374c54b519a4af893bb7850bbc819
AyseArucu <83887145+AyseArucu@users.noreply.github.com>
2026-02-20 16:10:40 +0300

    hnjm

--- ecommerce-platform/src/app/api/qr/create/route.ts
+++ ecommerce-platform/src/app/api/qr/create/route.ts
@@ -2,7 +2,6 @@ import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
 
-export const dynamic = "force-dynamic";
 
 export async function POST(req: Request) {
 
